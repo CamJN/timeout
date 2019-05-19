8fec6f9c44b6eb2af7b6d35d17a1f05ed06c4440
Camden Narzt <c.narzt@me.com>
2019-05-19 00:04:39 -0600

    compiling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::time::Duration;
+use std::process::Command;
+use futures::Future;
+use tokio::timer::Timeout;
+use tokio_process::CommandExt;
+use std::io::{stdout, Write};
+
+const TIMEOUT: u64 = 5;
+
 fn main() {
-    println!("Hello, world!");
+    let unlocked_stdout = stdout();
+    let mut stdout = unlocked_stdout.lock();
+    let mut args = std::env::args_os();
+    // 0th arg is path to self
+    assert!(args.len() > 1, "Arguments needed");
+
+    let command = args.next().unwrap();
+    let output = Command::new(command).args(args).output_async();
+
+    let future = output
+        .map_err(|e| panic!("failed to collect output: {}", e))
+        .map(|output| stdout.write(&output.stdout));
+
+    let _ =  Timeout::new(future, Duration::from_secs(TIMEOUT));
 }
