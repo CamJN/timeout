013febf969d6cb56ba1b9bb7d41edb97d34f9ab4
Camden Narzt <c.narzt@me.com>
2024-01-09 17:01:51 -0700

    update to remove vuln deps, and work with current tokio
    god tokio is horrific shit

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+use futures::future::FutureExt;
 use std::env::args_os;
 use std::os::unix::process::ExitStatusExt;
+use std::process::exit;
 use std::time::Duration;
-use std::process::{Command,exit};
-use tokio::timer::Timeout;
-use tokio_process::CommandExt;
-use futures::future::Future;
+use tokio::process::Command;
+use tokio::time::timeout;
 
 const TIMEOUT: u64 = 5;
 
-fn main() {
+#[tokio::main(flavor = "multi_thread", worker_threads = 1)]
+async fn main() {
     let default = "Failed to get panic message.";
     std::panic::set_hook(Box::new(move |p| {
         eprintln!("{}", p.payload().downcast_ref().unwrap_or(&default));
@@ -17,22 +18,34 @@ fn main() {
 
     // first arg is path to self
     let mut args = args_os().skip(1);
-    let mut command = args.next().expect("Arguments needed");
+    let mut command = args.next().expect("Timeout needed");
 
-    let limit = match command.as_os_str().to_str().map(str::parse) {
+    let limit = match command.to_str().map(str::parse) {
         Some(Ok(num)) => {
             command = args.next().expect("Command needed after timeout");
             num
-        },
-        _ => TIMEOUT
+        }
+        _ => TIMEOUT,
     };
 
-    let child = Command::new(command).args(args).spawn_async().expect("Failed to spawn").map(|status|if !status.success() {
-        match status.code() {
-            Some(code) => exit(code),
-            None       => panic!("Process terminated by signal {}",status.signal().unwrap())
-        }
-    });
-    let timeout = Timeout::new(child, Duration::from_secs(limit)).map_err(|_|panic!("Command timed out"));
-    tokio::run(timeout);
+    let mut child = Command::new(command)
+        .args(args)
+        .spawn()
+        .expect("Failed to spawn");
+
+        let future = child.wait()
+        .map(|rstatus| {
+            if let Ok(status) = rstatus {
+                match status.code() {
+                    Some(code) => exit(code),
+                    None => panic!("Process terminated by signal {}", status.signal().unwrap()),
+                }
+            }
+        });
+
+    let timeout = timeout(Duration::from_secs(limit), future);
+
+    if let Err(_) = timeout.await {
+        panic!("Command timed out");
+    }
 }
